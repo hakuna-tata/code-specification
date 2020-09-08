233afb4d4d64b1586329c52b2a1a565dd4628378
hakuna-tata <610578197@qq.com>
2020-09-09 01:30:26 +0800

    init specification

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+// npx eslint --init 初始化 .eslintrc.js 配置文件
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
