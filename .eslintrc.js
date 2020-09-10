8bfe4cdcc142b6946b48a076dcd6a6b4b956ff9a
hakuna-tata <610578197@qq.com>
2020-09-11 01:29:01 +0800

    eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,30 @@
 // npx eslint --init 初始化 .eslintrc.js 配置文件
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "es2021": true
+  root: true,
+  // 共享配置
+  extends: ['eslint:recommended'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  parser: '',
+  rules: {
+  },
+  globals: {
+    window: 'readonly',
+    define: 'readonly', // seajs keywords
+    mqq: 'writable'
+  },
 };
